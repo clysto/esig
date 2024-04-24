27fe5de5ab83af934f61356e860fdcac8559794a
毛亚琛 <maoyachen55@gmail.com>
2024-04-24 12:52:51 +0800

    fix window

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,14 @@ mod signal_plot;
 mod utils;
 
 fn main() -> eframe::Result<()> {
-    let native_options = eframe::NativeOptions::default();
+    let native_options = eframe::NativeOptions {
+        viewport: eframe::egui::ViewportBuilder::default()
+            .with_inner_size([1200.0, 800.0])
+            .with_min_inner_size([800.0, 600.0])
+            .with_title("ESig"),
+        centered: true,
+        ..Default::default()
+    };
     eframe::run_native(
         "esig",
         native_options,
--- src/open_dialog.rs
+++ src/open_dialog.rs
@@ -43,103 +43,96 @@ impl OpenDialog {
             .collapsible(false)
             .resizable(false)
             .default_width(300.)
-            .max_width(300.)
             .show(ctx, |ui| {
-                ui.vertical_centered_justified(|ui| {
-                    Grid::new("open-file-options")
-                        .num_columns(2)
-                        .striped(false)
-                        .show(ui, |ui| {
-                            ui.label("File");
-                            ui.with_layout(
-                                egui::Layout::right_to_left(egui::Align::Center),
-                                |ui| {
-                                    if ui.button("Browse").clicked() {
-                                        let path = rfd::FileDialog::new().pick_file();
-                                        if path.is_some() {
-                                            self.path = path.unwrap().to_str().unwrap().to_owned();
-                                        }
-                                    }
-                                    ui.with_layout(
-                                        egui::Layout::top_down_justified(egui::Align::LEFT),
-                                        |ui| {
-                                            egui::TextEdit::singleline(&mut self.path).ui(ui);
-                                        },
-                                    );
-                                },
-                            );
-                            ui.end_row();
-                            ui.label("Sample Rate");
+                ui.set_width(300.);
+                Grid::new("open-file-options")
+                    .num_columns(2)
+                    .max_col_width(200.)
+                    .spacing([40.0, 4.0])
+                    .show(ui, |ui| {
+                        ui.label("File");
+                        ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+                            if ui.button("Browse").clicked() {
+                                let path = rfd::FileDialog::new().pick_file();
+                                if path.is_some() {
+                                    self.path = path.unwrap().to_str().unwrap().to_owned();
+                                }
+                            }
                             ui.with_layout(
                                 egui::Layout::top_down_justified(egui::Align::LEFT),
                                 |ui| {
-                                    egui::DragValue::new(&mut self.sample_rate)
-                                        .custom_formatter(|f, _range| {
-                                            if f < 1_000.0 {
-                                                return format!("{:.0} Hz", f);
-                                            }
-                                            if f < 1_000_000.0 {
-                                                return format!("{} kHz", f / 1_000.0);
-                                            }
-                                            if f < 1_000_000_000.0 {
-                                                return format!("{} MHz", f / 1_000_000.0);
-                                            }
-                                            format!("{} GHz", f / 1_000_000_000.0)
-                                        })
-                                        .custom_parser(|str| {
-                                            // str 1000hz 1000mhz 1000 MHz 10GHz 200 GhZ is valid
-                                            let mut str = str.to_owned();
-                                            str.make_ascii_lowercase();
-                                            let num = str.trim_end_matches(|c: char| {
-                                                c.is_alphabetic() && c != 'e' && c != '.'
-                                            });
-                                            let mut unit = str.trim_start_matches(|c: char| {
-                                                c.is_numeric() || c == '.' || c == 'e'
-                                            });
-                                            if unit.is_empty() {
-                                                unit = "hz";
-                                            }
-                                            let num = num.parse::<f64>();
-                                            if num.is_err() {
-                                                return None;
-                                            }
-                                            let num = num.unwrap();
-                                            match unit {
-                                                "hz" => return Some(num),
-                                                "khz" => return Some(num * 1_000.0),
-                                                "mhz" => return Some(num * 1_000_000.0),
-                                                "ghz" => return Some(num * 1_000_000_000.0),
-                                                _ => return None,
-                                            }
-                                        })
-                                        .speed(1.0)
-                                        .ui(ui);
+                                    egui::TextEdit::singleline(&mut self.path).ui(ui);
                                 },
                             );
-                            ui.end_row();
-                            ui.label("Signal Type");
-                            egui::ComboBox::from_label("")
-                                .selected_text(match self.signal_type {
-                                    SignalType::Float32 => "float32",
-                                    SignalType::Complex64 => "complex64",
-                                })
-                                .width(ui.available_width())
-                                .show_ui(ui, |ui| {
-                                    ui.style_mut().wrap = Some(false);
-                                    ui.selectable_value(
-                                        &mut self.signal_type,
-                                        SignalType::Float32,
-                                        "float32",
-                                    );
-                                    ui.selectable_value(
-                                        &mut self.signal_type,
-                                        SignalType::Complex64,
-                                        "complex64",
-                                    );
-                                });
-                            ui.end_row();
                         });
-                });
+                        ui.end_row();
+                        ui.label("Sample Rate");
+                        ui.with_layout(egui::Layout::top_down_justified(egui::Align::LEFT), |ui| {
+                            egui::DragValue::new(&mut self.sample_rate)
+                                .custom_formatter(|f, _range| {
+                                    if f < 1_000.0 {
+                                        return format!("{:.0} Hz", f);
+                                    }
+                                    if f < 1_000_000.0 {
+                                        return format!("{} kHz", f / 1_000.0);
+                                    }
+                                    if f < 1_000_000_000.0 {
+                                        return format!("{} MHz", f / 1_000_000.0);
+                                    }
+                                    format!("{} GHz", f / 1_000_000_000.0)
+                                })
+                                .custom_parser(|str| {
+                                    // str 1000hz 1000mhz 1000 MHz 10GHz 200 GhZ is valid
+                                    let mut str = str.to_owned();
+                                    str.make_ascii_lowercase();
+                                    let num = str.trim_end_matches(|c: char| {
+                                        c.is_alphabetic() && c != 'e' && c != '.'
+                                    });
+                                    let mut unit = str.trim_start_matches(|c: char| {
+                                        c.is_numeric() || c == '.' || c == 'e'
+                                    });
+                                    if unit.is_empty() {
+                                        unit = "hz";
+                                    }
+                                    let num = num.parse::<f64>();
+                                    if num.is_err() {
+                                        return None;
+                                    }
+                                    let num = num.unwrap();
+                                    match unit {
+                                        "hz" => return Some(num),
+                                        "khz" => return Some(num * 1_000.0),
+                                        "mhz" => return Some(num * 1_000_000.0),
+                                        "ghz" => return Some(num * 1_000_000_000.0),
+                                        _ => return None,
+                                    }
+                                })
+                                .speed(1.0)
+                                .ui(ui);
+                        });
+                        ui.end_row();
+                        ui.label("Signal Type");
+                        egui::ComboBox::from_label("")
+                            .selected_text(match self.signal_type {
+                                SignalType::Float32 => "float32",
+                                SignalType::Complex64 => "complex64",
+                            })
+                            // .width(ui.available_width())
+                            .show_ui(ui, |ui| {
+                                ui.style_mut().wrap = Some(false);
+                                ui.selectable_value(
+                                    &mut self.signal_type,
+                                    SignalType::Float32,
+                                    "float32",
+                                );
+                                ui.selectable_value(
+                                    &mut self.signal_type,
+                                    SignalType::Complex64,
+                                    "complex64",
+                                );
+                            });
+                        ui.end_row();
+                    });
                 ui.add_space(30.);
                 ui.horizontal(|ui| {
                     ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
